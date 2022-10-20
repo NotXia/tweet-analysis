867158cb2e60019ee8556b22c542c6bb90b82085
Tian Cheng Xia <tiancheng.xia@studio.unibo.it>
2022-10-20 17:16:22 +0200

    Aggiunte cartelle
    Co-authored-by: Zaid Cheikh <zaid.cheikhibrahim@studio.unibo.it>
    Co-authored-by: Lee Henry <qunhaohaohenry.lee@studio.unibo.it>
    Co-authored-by: Manuel Paris <manuel.paris@studio.unibo.it>

---
+++ middlewares/test.js
@@ -0,0 +1,7 @@
+function test(req, res, next) {
+    next();
+}
+
+module.exports = {
+    test: test
+}
--- routes/test.js
+++ routes/test.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
 
+const middleware = require("../middlewares/test");
 const controller = require("../controllers/test");
 
-router.get("/1", controller.test);
+router.get("/1", middleware.test, controller.test);
 
 module.exports = router;
