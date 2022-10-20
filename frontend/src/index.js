b6d91909f8e729601e40f02387add069e2af0b58
Tian Cheng Xia <tiancheng.xia@studio.unibo.it>
2022-10-20 18:34:43 +0200

    Aggiunto gestione basepath

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,12 +4,11 @@ import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import home_routes from "./pages/Home/routes";
 
-
 let routes = [];
 routes = routes.concat(home_routes);
 
 routes.push({ path: "/*", loader: () => { window.location.href="/not-found.html" } }); // Gestione not found (lasciare come ultimo route)
-const router = createBrowserRouter(routes);
+const router = createBrowserRouter(routes, { basename: process.env.REACT_APP_BASE_PATH });
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
