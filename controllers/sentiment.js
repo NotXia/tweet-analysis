a41026bbf215b9a4b86267c3100234481d5e5260
NotXia <tiancheng.xia@studio.unibo.it>
2022-10-23 20:15:19 +0200

    Add try catch

--- controllers/sentiment.js
+++ controllers/sentiment.js
@@ -2,14 +2,20 @@ const sentiment = require("../modules/sentiment.js");
 
 
 async function sentimentAnalysis(req, res) {
-    let options = {}
+    let options = {};
+    let sentiment_result;
 
     // Composizione opzioni
     if (req.query.lang) options.language = req.query.lang;
     if (req.query.bias) options.bias = req.query.bias;
-
-    // Analisi del tweet
-    const sentiment_result = await sentiment(req.query.tweet, options);
+    
+    try {
+        // Analisi del tweet
+        sentiment_result = await sentiment(req.query.tweet, options);
+    }
+    catch (err) {
+        res.sendStatus(500);
+    }
 
     res.status(200).json({ 
         sentiment: sentiment_result.sentiment,
