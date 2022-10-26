b72796c92b4293e761a530e6dc4ab38279ba76bf
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-10-27 01:08:57 +0200

    Update hashtagFetch

--- modules/fetch/hashtag.js
+++ modules/fetch/hashtag.js
@@ -4,14 +4,22 @@ const axios = require('axios');
 module.exports = hashtagFetch;
 
 async function hashtagFetch(hashtag) {
-    // TODO: controllo dell'input (loLowerCase e togliere eventuale # a inizio stringa)    
-    let tweets = await axios({
-        method: "get",
-        url: `https://api.twitter.com/2/tweets/search/recent?query=${encodeURIComponent("#")}${hashtag}`,
-        headers: { Authorization: `Bearer ${process.env.BEARER_TOKEN}` }
+    // TODO: controllo dell'input (loLowerCase e togliere eventuale # a inizio stringa)
+
+    let tweets = await axios.get(`https://api.twitter.com/2/tweets/search/recent`, {
+        headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
+        params: {
+            query: `#${hashtag}`,
+            "max_results": 100,
+            "tweet.fields": "created_at,geo,text,public_metrics",
+            "expansions": "author_id",
+            "place.fields": "country,full_name",
+            "media.fields": "url,variants",
+            "user.fields": "name,profile_image_url,username"
+        }
     });
 
-    // console.log(tweets.data.data[0]);
+    // console.log(tweets.data.includes);
 
     return tweets;
 }
