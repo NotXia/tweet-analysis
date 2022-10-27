6917ae403cdb7fb1f6c9ecb6a82d95aef702f973
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-10-27 10:12:08 +0200

    Add function normalizeHashtag and remove try-catch

--- modules/fetch/hashtag.js
+++ modules/fetch/hashtag.js
@@ -5,27 +5,36 @@ module.exports = hashtagFetch;
 
 async function hashtagFetch(hashtag) {
     // TODO: controllo dell'input (loLowerCase e togliere eventuale # a inizio stringa)
-    try {
-        let tweets = await axios.get(`https://api.twitter.com/2/tweets/search/recent`, {
-        headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
-        params: {
-            query: `#${hashtag}`,
-            "max_results": 10,
-            "tweet.fields": "created_at,geo,text,public_metrics,attachments",
-            "expansions": "author_id,attachments.media_keys",
-            "place.fields": "country,full_name",
-            "media.fields": "url,variants",
-            "user.fields": "name,profile_image_url,username"
+    hashtag = normalizeHashtag(hashtag);
+
+    let tweets = await axios.get(`https://api.twitter.com/2/tweets/search/recent`, {
+    headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
+    params: {
+        query: `#${hashtag}`,
+        "max_results": 10,                                                  // Numero massimo Tweet per pagina
+        "tweet.fields": "created_at,geo,text,public_metrics,attachments",   // Campi del Tweet
+        "expansions": "author_id,attachments.media_keys",                   // Espansioni del campo Tweet
+        "place.fields": "country,full_name",                                // Campi della località
+        "media.fields": "url,variants",                                     // Campi degli allegati
+        "user.fields": "name,profile_image_url,username"                    // Campi dell'utente
+    }
+    });
+
+    // console.log(tweets.data.includes.media[0]);
+
+    // console.log(tweets.data.data);
+    // console.log(tweets.data.includes);
+
+    return tweets;
+}
+
+function normalizeHashtag(hashtag) {
+    if(hashtag.length !== 0) {
+        if(hashtag[0] == '#') {
+            hashtag = hashtag.slice(1);     // Se la stringa inizia con #, viene rimosso
         }
-        });
+        hashtag = hashtag.toLowerCase();    // Imposta tutta la stringa a lower case
 
-        console.log(tweets.data.includes.media[0]);
-
-        // console.log(tweets.data.data);
-        // console.log(tweets.data.includes);
-
-        return tweets;
-    } catch (error) {
-        console.log(error);
+        return hashtag;
     }
 }
