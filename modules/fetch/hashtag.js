c940b316c25633aaf447a672e52cb9c8db676b2b
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-10-27 20:28:22 +0200

    Add function hashtagFetch

--- modules/fetch/hashtag.js
+++ modules/fetch/hashtag.js
@@ -1,33 +1,50 @@
 require("dotenv").config();
 const axios = require('axios');
 
-module.exports = hashtagFetch;
+module.exports = getTweetsByHashtag;
 
-async function hashtagFetch(hashtag) {
-    hashtag = normalizeHashtag(hashtag);
-
-    let tweets = await axios.get(`https://api.twitter.com/2/tweets/search/recent`, {
-    headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
-    params: {
-        query: `#${hashtag}`,
-        "max_results": 10,                                                  // Numero massimo Tweet per pagina
-        "tweet.fields": "created_at,geo,text,public_metrics,attachments",   // Campi del Tweet
-        "expansions": "author_id,attachments.media_keys",                   // Espansioni del campo Tweet
-        "place.fields": "country,full_name",                                // Campi della località
-        "media.fields": "url,variants",                                     // Campi degli allegati
-        "user.fields": "name,profile_image_url,username"                    // Campi dell'utente
+/* istanbul ignore next */
+if (process.env.NODE_ENV === "testing") {
+    module.exports = {
+        getTweetsByHashtag: getTweetsByHashtag,
+        normalizeHashtag: _normalizeHashtag,
+        hashtagFetch: _hashtagFetch
     }
-    });
+}
 
-    // console.log(tweets.data.includes.media[0]);
+async function getTweetsByHashtag(hashtag, pagination_token="") {
+    tweets = _hashtagFetch(hashtag);
 
-    // console.log(tweets.data.data);
-    // console.log(tweets.data.includes);
+    console.log(tweets.data);
 
     return tweets;
 }
 
-function normalizeHashtag(hashtag) {
+async function _hashtagFetch(hashtag, pagination_token="") {
+    hashtag = _normalizeHashtag(hashtag);
+
+    let options = {
+        headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
+        params: {
+            query: `#${hashtag}`,
+            "max_results": 10,                                                  // Numero massimo Tweet per pagina
+            "tweet.fields": "created_at,geo,text,public_metrics,attachments",   // Campi del Tweet
+            "expansions": "author_id,attachments.media_keys",                   // Espansioni del campo Tweet
+            "place.fields": "country,full_name",                                // Campi della località
+            "media.fields": "url,variants",                                     // Campi degli allegati
+            "user.fields": "name,profile_image_url,username"                    // Campi dell'utente
+        }
+    };
+    if (pagination_token != "") {
+        options.params["pagination_token"] = pagination_token;
+    }
+
+    let fetchedTweets = await axios.get(`https://api.twitter.com/2/tweets/search/recent`, options);
+
+    return fetchedTweets;
+}
+
+function _normalizeHashtag(hashtag) {
     if(hashtag.length !== 0) {
         if(hashtag[0] == '#') {
             hashtag = hashtag.slice(1);     // Se la stringa inizia con #, viene rimosso
