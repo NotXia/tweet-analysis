ee814ce4efde2ea273836d0c53aef265ccff2cbb
Manuel Paris <manuel.paris@studio.unibo.it>
2022-10-29 21:12:22 +0200

    Fix middleware user

--- middlewares/user.js
+++ middlewares/user.js
@@ -1,13 +1,13 @@
 const httpError = require("http-errors");
 
-const tweetsByUser = {
+const tweetsByUser = [
     function (req, res, next) {     //Validazione dell'input
 
         if (!req.query.user){ return next(httpError(400, JSON.stringify({ user: 'Nome utente mancante' }))); }
         if (!req.query.pag_token) { req.query.pag_token = ''; }
         return next();
     }
-};
+];
 
 module.exports = {
     tweetsByUserValidation: tweetsByUser
