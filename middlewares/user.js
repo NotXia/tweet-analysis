5d3791fcb83c4517bef16d2aab482738cc0f9ba9
Manuel Paris <manuel.paris@studio.unibo.it>
2022-10-29 20:59:42 +0200

    Fix API ricerca utente

--- middlewares/user.js
+++ middlewares/user.js
@@ -3,7 +3,7 @@ const httpError = require("http-errors");
 const tweetsByUser = {
     function (req, res, next) {     //Validazione dell'input
 
-        if (!req.query.user){ return next(httpError(400, 'Nome utente mancante')); }
+        if (!req.query.user){ return next(httpError(400, JSON.stringify({ user: 'Nome utente mancante' }))); }
         if (!req.query.pag_token) { req.query.pag_token = ''; }
         return next();
     }
