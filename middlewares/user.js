93a9869a5dc213b8efc1bb497727c2430cd69b91
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-10-29 21:28:25 +0200

    Fix

--- middlewares/user.js
+++ middlewares/user.js
@@ -3,8 +3,8 @@ const httpError = require("http-errors");
 const tweetsByUser = [
     function (req, res, next) {     //Validazione dell'input
 
-        if (!req.query.user){ return next(httpError(400, JSON.stringify({ user: 'Nome utente mancante' }))); }
-        if (!req.query.pag_token) { req.query.pag_token = ''; }
+        if (!req.query.user)        { return next(httpError(400, JSON.stringify({ user: 'Nome utente mancante' }))); }
+        if (!req.query.pag_token)   { req.query.pag_token = ''; }
         return next();
     }
 ];
