2be6f937aa99ded6bada10c638be72e3e226a8cb
NotXia <tiancheng.xia@studio.unibo.it>
2022-10-30 22:46:39 +0100

    Add messaggio di errore

--- frontend/src/pages/SearchTweets/index.jsx
+++ frontend/src/pages/SearchTweets/index.jsx
@@ -13,7 +13,9 @@ class SearchUser extends React.Component {
         super(props);
         this.state = {
             tweets: [],
-            page: ""
+            page: "",
+
+            error_message: ""
         };
 
         this.input = {
@@ -29,43 +31,52 @@ class SearchUser extends React.Component {
             
             <Navbar />
 
-            <form className="row align-items-start p-4" onSubmit={(e) => { this.fetchUserTweets(e) }}>
-                <div className="col-4">
-                    <div className="input-group flex-nowrap">
-                        {/* <span className="input-group-text bg-white" id="addon-wrapping">@</span> */}
-                        <input ref={this.input.username} className="form-control" type="text" placeholder="Ricerca" aria-label="Username" />
-                        <input className="input-group-text bg-white" type="submit"/>
-                    </div>
-                    <p className="ms-1" style={{ fontSize: "0.9rem" }}>Ricerca per hashtag (#) o nome utente (@)</p>
+            <main className="mt-4">
+                <div className="text-center text-danger fw-semibold">
+                    { this.state.error_message}
                 </div>
-            </form>
 
-            <div className="list-group col-4 ms-4 my-2">
-                {
-                    this.state.tweets.map((tweet) => (
-                        <Tweet key={tweet.id} tweet={tweet} />
-                    ))
-                }
-            </div>
+                <form className="row align-items-start px-4" onSubmit={(e) => { this.fetchUserTweets(e) }}>
+                    <div className="col-4">
+                        <div className="input-group flex-nowrap">
+                            {/* <span className="input-group-text bg-white" id="addon-wrapping">@</span> */}
+                            <input ref={this.input.username} className="form-control" type="text" placeholder="Ricerca" aria-label="Username" />
+                            <input className="input-group-text bg-white" type="submit"/>
+                        </div>
+                        <p className="ms-1" style={{ fontSize: "0.9rem" }}>Ricerca per hashtag (#) o nome utente (@)</p>
+                    </div>
+                </form>
 
-            
+                <div className="list-group col-4 ms-4 my-2">
+                    {
+                        this.state.tweets.map((tweet) => (
+                            <Tweet key={tweet.id} tweet={tweet} />
+                        ))
+                    }
+                </div>
+            </main>
         </>);
     }
 
     async fetchUserTweets(e) {
         e.preventDefault()
         
-        const query = this.input.username.current.value.trim();
-        let tweets_data = [];
-
-        if (query[0] === "@") { tweets_data = await userSearchTweet(query); }
-        else if (query[0] === "#") { tweets_data = await hashtagSearchTweet(query) }
-        else { return; }
-
-        this.setState({ 
-            tweets: tweets_data.tweets,
-            page: tweets_data.token
-        })
+        try {
+            const query = this.input.username.current.value.trim();
+            let tweets_data = [];
+    
+            if (query[0] === "@") { tweets_data = await userSearchTweet(query); }
+            else if (query[0] === "#") { tweets_data = await hashtagSearchTweet(query) }
+            else { return; }
+    
+            this.setState({ 
+                tweets: tweets_data.tweets,
+                page: tweets_data.token
+            })
+        }
+        catch (err) {
+            this.setState({ error_message: "Si è verificato un errore durante la ricerca" });
+        }
     }
 }
 
