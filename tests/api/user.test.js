65372c1ac6bc548c475c9936aca0d5948c86c9f1
Manuel Paris <manuel.paris@studio.unibo.it>
2022-10-30 17:28:33 +0100

    Fix ricerca utente

--- tests/api/user.test.js
+++ tests/api/user.test.js
@@ -82,6 +82,81 @@ describe("Richieste corrette a /tweets/user", function () {
         }
         expect( res.body.next_token ).toBeDefined();
     });
+
+    test("Tweet dato username con spazi", async function () {
+        const res = await curr_session.get("/tweets/user").query({ user: "  wwe    " }).expect(200);
+        expect( res.body.tweets ).toBeDefined();
+        expect( res.body.tweets.length ).toBeLessThanOrEqual(10);
+        for(const tweet of res.body.tweets) {
+            expect( tweet ).toBeDefined();
+            expect( tweet.id ).toBeDefined();
+            expect( tweet.name ).toBeDefined();
+            expect( tweet.username ).toBeDefined();
+            expect( tweet.username.toLowerCase() ).toEqual("wwe");
+            expect( tweet.pfp ).toBeDefined();
+            expect( tweet.text ).toBeDefined();
+            expect( tweet.time ).toBeDefined();
+            expect( tweet.likes ).toBeDefined();
+            expect( tweet.likes ).not.toBeNaN();
+            expect( tweet.comments ).toBeDefined();
+            expect( tweet.comments ).not.toBeNaN();
+            expect( tweet.retweets ).toBeDefined();
+            expect( tweet.retweets ).not.toBeNaN();
+            expect( tweet.media ).toBeDefined();
+            expect(Array.isArray(tweet.media)).toBe(true);
+        }
+        expect( res.body.next_token ).toBeDefined();
+    });
+
+    test("Tweet dato username con @ all'inizio", async function () {
+        const res = await curr_session.get("/tweets/user").query({ user: "@wwe" }).expect(200);
+        expect( res.body.tweets ).toBeDefined();
+        expect( res.body.tweets.length ).toBeLessThanOrEqual(10);
+        for(const tweet of res.body.tweets) {
+            expect( tweet ).toBeDefined();
+            expect( tweet.id ).toBeDefined();
+            expect( tweet.name ).toBeDefined();
+            expect( tweet.username ).toBeDefined();
+            expect( tweet.username.toLowerCase() ).toEqual("wwe");
+            expect( tweet.pfp ).toBeDefined();
+            expect( tweet.text ).toBeDefined();
+            expect( tweet.time ).toBeDefined();
+            expect( tweet.likes ).toBeDefined();
+            expect( tweet.likes ).not.toBeNaN();
+            expect( tweet.comments ).toBeDefined();
+            expect( tweet.comments ).not.toBeNaN();
+            expect( tweet.retweets ).toBeDefined();
+            expect( tweet.retweets ).not.toBeNaN();
+            expect( tweet.media ).toBeDefined();
+            expect(Array.isArray(tweet.media)).toBe(true);
+        }
+        expect( res.body.next_token ).toBeDefined();
+    });
+
+    test("Tweet dato username con @, spazi, e casing diverso", async function () {
+        const res = await curr_session.get("/tweets/user").query({ user: " @ wwE    " }).expect(200);
+        expect( res.body.tweets ).toBeDefined();
+        expect( res.body.tweets.length ).toBeLessThanOrEqual(10);
+        for(const tweet of res.body.tweets) {
+            expect( tweet ).toBeDefined();
+            expect( tweet.id ).toBeDefined();
+            expect( tweet.name ).toBeDefined();
+            expect( tweet.username ).toBeDefined();
+            expect( tweet.username.toLowerCase() ).toEqual("wwe");
+            expect( tweet.pfp ).toBeDefined();
+            expect( tweet.text ).toBeDefined();
+            expect( tweet.time ).toBeDefined();
+            expect( tweet.likes ).toBeDefined();
+            expect( tweet.likes ).not.toBeNaN();
+            expect( tweet.comments ).toBeDefined();
+            expect( tweet.comments ).not.toBeNaN();
+            expect( tweet.retweets ).toBeDefined();
+            expect( tweet.retweets ).not.toBeNaN();
+            expect( tweet.media ).toBeDefined();
+            expect(Array.isArray(tweet.media)).toBe(true);
+        }
+        expect( res.body.next_token ).toBeDefined();
+    });
 });
 
 describe("Richieste errate a /tweets/user", function () {
