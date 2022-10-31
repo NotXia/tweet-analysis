03c25aaf008023bd295db62661f144159b06dd20
NotXia <tiancheng.xia@studio.unibo.it>
2022-10-31 12:17:49 +0100

    Add modulo per rimuovere punteggiatura

---
+++ modules/analysis/utils/removePunctuation.js
@@ -0,0 +1,13 @@
+module.exports = {
+    removePunctuation: removePunctuation
+}
+
+/**
+ * Rimuove la punteggiatura da una stringa
+ * @param {string} sentence     Stringa da elaborare
+ * @returns {string} Frase senza punteggiatura e senza spazi multipli
+ */
+function removePunctuation(sentence) {
+    return sentence.replace(/[.,\/#!$%\^&\*;:{}=\-_`'"~()]/g," ") // Rimuove punteggiatura
+                   .replace(/\s{2,}/g," ");                       // Collassa multipli spazi in uno solo
+}
