532b08f4916459708dac3c4c4728bed104292895
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-11-04 20:08:35 +0100

    Add doc

--- modules/fetch/multiple_tweets.js
+++ modules/fetch/multiple_tweets.js
@@ -1,15 +1,23 @@
 module.exports = { multipleTweetsFetch: multipleTweetsFetch };
 
+/**
+ * Restituisce gli ultimi tweet data una funzione di fetch e relativi parametri 
+ * @param {function} fetcher                Funzione dalla quale fare fetch dei tweet
+ * @param {string} query                    Query da ricercare
+ * @param {string} pagination_token         Token della pagina successiva
+ * @param {number} quantity                 Quantità di tweet da ricercare
+ * @returns Una pagina di dimensione richiesta di tweet
+ */
 async function multipleTweetsFetch(fetcher, query, pagination_token="", quantity=10) {
     let fetchedTweets = [];
 
     while (quantity > 0) {
         try {
-            if (quantity < 10) { quantity = 10; } 
-            const tweets = await fetcher(query, pagination_token, quantity < 100 ? quantity : 100);
-            fetchedTweets = fetchedTweets.concat(tweets.tweets);
-            pagination_token = tweets.next_token;
-            quantity -= 100;
+            if (quantity < 10) { quantity = 10; }                                                           // Imposta la quantità a 10, ovvero il minimo per le API di Twitter
+            const tweets = await fetcher(query, pagination_token, quantity < 100 ? quantity : 100);         // Viene richiamato il fetcher con query, pagination_token e quantità. La quantità è 100 se rimangono ancora più di 100 tweet da ricercare.
+            fetchedTweets = fetchedTweets.concat(tweets.tweets);                                            // I tweet fetchati vengono concatenati a quelli precedenti
+            pagination_token = tweets.next_token;                                                           // Viene aggiornato il pagination_token
+            quantity -= 100;                                                                                // Viene ridotta la quantità di 100 a prescindere vista la condizione del while
         } catch (err) {
             pagination_token = "";
             break;
