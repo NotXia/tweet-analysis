0541f75bf9fdae848c8719509ec924e71e217f40
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-11-04 21:53:42 +0100

    Update API controller

--- controllers/hashtag.js
+++ controllers/hashtag.js
@@ -1,11 +1,13 @@
+const { multipleTweetsFetch } = require("../modules/fetch/multiple_tweets.js");
 const { getTweetsByHashtag } = require("../modules/fetch/hashtag.js");
 
 async function tweetsByHashtag(req, res) {
     let tweets_response;
 
     try {
-        tweets_response = await getTweetsByHashtag(req.query.hashtag, req.query.pag_token);
+        tweets_response = await multipleTweetsFetch(getTweetsByHashtag, req.query.hashtag, req.query.pag_token, req.query.quantity);
     } catch (error) {
+        console.log(error);
         res.sendStatus(500);
         return;
     }
