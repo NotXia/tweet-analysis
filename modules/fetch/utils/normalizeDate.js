f57174e093ca1a90b36fb0bf6e0cf4b8caac68bf
Manuel Paris <manuel.paris@studio.unibo.it>
2022-11-05 22:52:51 +0100

    Update commenti

--- modules/fetch/utils/normalizeDate.js
+++ modules/fetch/utils/normalizeDate.js
@@ -12,20 +12,21 @@ module.exports = {
 function _normalizeDate(start_time = '', end_time = '') {
 
     let today = new Date();
+    let limit = new Date('2010-11-06T00:00:01Z');
+
     if (start_time != '') {
         start_time = new Date(start_time);
         start_time.setUTCHours(0,0,1,0);
         today.setUTCHours(0,0,1,0);
-        let limit = new Date('2010-11-06T00:00:01Z');
-        if (start_time < limit) { start_time = ''; }
-        else if (start_time.getTime() !== today.getTime() && start_time > today) { throw new Error('Data di inizio non valida') }
+        if (start_time < limit) { start_time = ''; }   // Se la data di inizio è prima del limite massimo di twitter, non viene impostata (quindi é illimitata)
+        else if (start_time.getTime() !== today.getTime() && start_time > today) { throw new Error('Data di inizio non valida') }  // Se la data di inizio è nel futuro, viene lanciato un errore
     }
     if (end_time != '') { 
         end_time = new Date(end_time);
         end_time.setUTCHours(23,59,59,0);
         today.setUTCHours(23,59,59,0);
-        if (end_time.getTime() === today.getTime()) { end_time = ''; }
-        else if (end_time > today) { throw new Error('Data di fine non valida') }
+        if (end_time.getTime() === today.getTime()) { end_time = ''; }   // Se la data di fine è oggi, non serve impostarla
+        else if (end_time < limit || end_time > today) { throw new Error('Data di fine non valida') }   // Se la data di fine è prima del limite massimo di twitter o nel futuro, viene lanciato un errore
     }
     
     if ((start_time != '' && end_time != '') && (end_time < start_time)) { throw new Error('Intervallo temporale non valido')}
