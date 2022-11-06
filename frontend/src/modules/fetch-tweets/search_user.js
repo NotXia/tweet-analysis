1454dd94e396995184c2209d0726b85cca9ed32a
lee_henry <qunhaohaohenry.lee@studio.unibo.it>
2022-11-06 14:32:57 +0100

    Preparazione al merge dev

--- frontend/src/modules/fetch-tweets/search_user.js
+++ frontend/src/modules/fetch-tweets/search_user.js
@@ -4,19 +4,17 @@ import axios from "axios";
 /**
  * Ritorna il risultato della ricerca eseguita
  * @param {String} user                 Utente a cui prendere i tweet
- * @param {string} pag_token            Token della pagina dei tweet (default "")
- * @param {number} quantity             Numero di tweet da ricercare (default 10)
+ * @param {string} pag_token            Token della pagina dei tweet (default undefined)
  * @returns I tweet di output a seconda dei parametri inviati
  */
 
 
-export async function userSearchTweet(user, pag_token="", quantity=10) {
+export async function userSearchTweet(user, pag_token="") {
     const res = await axios({
         method: "GET", url: `${process.env.REACT_APP_API_PATH}/tweets/user`,
         params: {
             user: user,
             pag_token: pag_token,
-            quantity: quantity
         }
     });
 
