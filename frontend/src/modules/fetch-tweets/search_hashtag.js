e810e868e8376d642751e22f8ddb58583f5ed1c8
Henry Lee <qunhaohaohenry.lee@studio.unibo.it>
2022-11-06 13:50:18 +0000

    Revert "Follow up commit sotto"
    This reverts commit ddd7f84fd3c9f7c782636738b637f74785bfb047

--- frontend/src/modules/fetch-tweets/search_hashtag.js
+++ frontend/src/modules/fetch-tweets/search_hashtag.js
@@ -5,16 +5,18 @@ import axios from "axios";
  * Ritorna il risultato della ricerca eseguita
  * @param {String} hashtag              Hashtag da cui prendere i tweet
  * @param {string} pag_token            Token della pagina dei tweet (default "")
+ * @param {number} quantity             Numero di tweet da ricercare (default 10)
  * @returns I tweet di output a seconda dei parametri inviati
  */
 
 
-export async function hashtagSearchTweet(hashtag, pag_token="") {
+export async function hashtagSearchTweet(hashtag, pag_token="", quantity=10) {
     const res = await axios({
         method: "GET", url: `${process.env.REACT_APP_API_PATH}/tweets/hashtag`,
         params: {
             hashtag: hashtag,
             pag_token: pag_token,
+            quantity: quantity
         }
     });
 
