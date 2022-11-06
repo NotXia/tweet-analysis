cdc044109a71f050c57fa3046ba9d2e23a4a5fed
Manuel Paris <manuel.paris@studio.unibo.it>
2022-11-06 19:04:56 +0100

    Fix normalizeDate

--- modules/fetch/utils/normalizeDate.js
+++ modules/fetch/utils/normalizeDate.js
@@ -16,15 +16,15 @@ function _normalizeDate(limit, start_time = '', end_time = '') {
 
     if (start_time != '') {
         start_time = new Date(start_time);
-        start_time.setUTCHours(0,0,1,0);
-        today.setUTCHours(0,0,1,0);
+        start_time.setUTCHours(0,0,0,0);
+        today.setUTCHours(0,0,0,0);
         if (start_time < limit) { start_time = ''; }   // Se la data di inizio è prima del limite massimo di twitter, non viene impostata (quindi é illimitata)
         else if (start_time.getTime() !== today.getTime() && start_time > today) { throw new Error('Data di inizio non valida') }  // Se la data di inizio è nel futuro, viene lanciato un errore
     }
     if (end_time != '') { 
         end_time = new Date(end_time);
-        end_time.setUTCHours(23,59,59,0);
-        today.setUTCHours(23,59,59,0);
+        end_time.setUTCHours(23,59,59,999);
+        today.setUTCHours(23,59,59,999);
         if ((end_time.getTime() === today.getTime()) || end_time > today) { end_time = ''; }   // Se la data di fine è oggi o nel futuro, non viene impostata (quindi i tweet arriveranno a oggi)
         else if (end_time < limit) { throw new Error('Data di fine non valida') }   // Se la data di fine è prima del limite massimo di twitter, viene lanciato un errore
     }
