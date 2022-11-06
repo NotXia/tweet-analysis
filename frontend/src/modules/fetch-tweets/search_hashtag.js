ddd7f84fd3c9f7c782636738b637f74785bfb047
lee_henry <qunhaohaohenry.lee@studio.unibo.it>
2022-11-06 14:34:39 +0100

    Follow up commit sotto

--- frontend/src/modules/fetch-tweets/search_hashtag.js
+++ frontend/src/modules/fetch-tweets/search_hashtag.js
@@ -5,18 +5,16 @@ import axios from "axios";
  * Ritorna il risultato della ricerca eseguita
  * @param {String} hashtag              Hashtag da cui prendere i tweet
  * @param {string} pag_token            Token della pagina dei tweet (default "")
- * @param {number} quantity             Numero di tweet da ricercare (default 10)
  * @returns I tweet di output a seconda dei parametri inviati
  */
 
 
-export async function hashtagSearchTweet(hashtag, pag_token="", quantity=10) {
+export async function hashtagSearchTweet(hashtag, pag_token="") {
     const res = await axios({
         method: "GET", url: `${process.env.REACT_APP_API_PATH}/tweets/hashtag`,
         params: {
             hashtag: hashtag,
             pag_token: pag_token,
-            quantity: quantity
         }
     });
 
