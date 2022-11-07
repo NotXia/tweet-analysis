d11fd75bcc964833027046fabccd42038f82a0a7
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-11-07 21:39:48 +0100

    Update keyword hashtag api test

--- tests/api/hashtag.test.js
+++ tests/api/hashtag.test.js
@@ -23,9 +23,9 @@ let date2 = new Date();
 date2 = new Date(moment(date2).subtract(1, 'days'));
 date2.setHours(23,59,59,999);
 
-describe("Richieste corrette a /tweets/hashtag", function () {
+describe("Richieste corrette a /tweets/keyword", function () {
     test("Tweet dato solo hashtag", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#leredita" }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#leredita" }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         expect( res.body.tweets.length ).toBeLessThanOrEqual(10);
         for(const tweet of res.body.tweets) {
@@ -50,7 +50,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet dato hashtag e pagination token", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#leredita", pag_token: pagination_token }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#leredita", pag_token: pagination_token }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         expect( res.body.tweets.length ).toBeLessThanOrEqual(10);
         for(const tweet of res.body.tweets) {
@@ -76,7 +76,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     test("Tweet dato hashtag, pagination token e quantità", async function () {
         const query = "#reazioneacatena";
         const max_results = await getCountRecentHashtagTweets(query);
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: query, pag_token: pagination_token, quantity: 50 }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: query, pag_token: pagination_token, quantity: 50 }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         expect( res.body.tweets.length ).toBeLessThanOrEqual(max_results);
         for(const tweet of res.body.tweets) {
@@ -100,8 +100,8 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet dato hashtag con spazi", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: " l eredita " }).expect(200);
-        const res2 = await curr_session.get("/tweets/hashtag").query({ hashtag: "leredita" });
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: " l eredita " }).expect(200);
+        const res2 = await curr_session.get("/tweets/keyword").query({ keyword: "leredita" });
         expect( res.body.tweets ).toBeDefined();
         expect( res.body.tweets ).toEqual( res2.body.tweets );
         expect( res.body.next_token ).toBeDefined();
@@ -109,7 +109,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con date valide", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: 20, start_time: date1, end_time: date2 }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: 20, start_time: date1, end_time: date2 }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         for (const tweet of res.body.tweets) {
             const time = new Date(tweet.time);
@@ -119,7 +119,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con solo data d'inizio", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: 20, start_time: date1 }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: 20, start_time: date1 }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         for (const tweet of res.body.tweets) {
             const time = new Date(tweet.time);
@@ -128,7 +128,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con solo data di fine", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: 20, start_time: '', end_time: date2 }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: 20, start_time: '', end_time: date2 }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         for (const tweet of res.body.tweets) {
             const time = new Date(tweet.time);
@@ -137,7 +137,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con data di inizio prima del limite", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: 20, start_time: '2022-10-06T00:00:01Z' }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: 20, start_time: '2022-10-06T00:00:01Z' }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         for (const tweet of res.body.tweets) {
             const time = new Date(tweet.time);
@@ -146,7 +146,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con data di fine nel futuro", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: 20, start_time: '', end_time: future }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: 20, start_time: '', end_time: future }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         for (const tweet of res.body.tweets) {
             const time = new Date(tweet.time);
@@ -155,7 +155,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con date nello stesso giorno", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: 20, start_time: date1, end_time: date1 }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: 20, start_time: date1, end_time: date1 }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         let date1_end = new Date();
         date1_end = new Date(moment(date1_end).subtract(5, 'days'));
@@ -168,7 +168,7 @@ describe("Richieste corrette a /tweets/hashtag", function () {
     });
 
     test("Tweet in intervallo temporale con data di inizio e data di fine a oggi", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#wwe", pag_token: '', quantity: 20, start_time: today, end_time: today }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#wwe", pag_token: '', quantity: 20, start_time: today, end_time: today }).expect(200);
         expect( res.body.tweets ).toBeDefined();
         const today_start = new Date();
         today_start.setHours(0,0,0,0);
@@ -181,45 +181,45 @@ describe("Richieste corrette a /tweets/hashtag", function () {
 });
 
 
-describe("Richieste errate a /tweets/hashtag", function () {
+describe("Richieste errate a /tweets/keyword", function () {
     test("Tweet senza hashtag e senza pagination token", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ }).expect(400);
+        const res = await curr_session.get("/tweets/keyword").query({ }).expect(400);
         expect( res.body.tweets ).not.toBeDefined();
         expect( res.body.next_token ).not.toBeDefined();
     });
 
     test("Tweet senza hashtag", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ pag_token: pagination_token }).expect(400);
+        const res = await curr_session.get("/tweets/keyword").query({ pag_token: pagination_token }).expect(400);
         expect( res.body.tweets ).not.toBeDefined();
         expect( res.body.next_token ).not.toBeDefined();
     });
 
     test("Tweet con hashtag senza tweet", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#uniboswe39393948aaa999zed" }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#uniboswe39393948aaa999zed" }).expect(200);
         expect( res.body.tweets.length ).toEqual(0);
         expect( res.body.next_token ).toEqual("");
     });
 
     test("Tweet con token errato", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: "123456789" }).expect(200);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: "123456789" }).expect(200);
         expect( res.body.tweets.length ).toEqual(0);
         expect( res.body.next_token ).toEqual("");
     });
 
     test("Tweet in intervallo temporale con data di fine prima di data d'inizio", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: '', start_time: date2, end_time: date1 }).expect(400);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: '', start_time: date2, end_time: date1 }).expect(400);
         expect( res.body.tweets ).not.toBeDefined();
         expect( res.body.next_token ).not.toBeDefined();
     });
 
     test("Tweet in intervallo temporale con data di inizio nel futuro", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: '', start_time: future }).expect(400);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: '', start_time: future }).expect(400);
         expect( res.body.tweets ).not.toBeDefined();
         expect( res.body.next_token ).not.toBeDefined();
     });
 
     test("Tweet in intervallo temporale con data di fine prima del limite", async function () {
-        const res = await curr_session.get("/tweets/hashtag").query({ hashtag: "#reazioneacatena", pag_token: '', quantity: '', start_time: '', end_time: '2022-10-06T00:00:01Z' }).expect(400);
+        const res = await curr_session.get("/tweets/keyword").query({ keyword: "#reazioneacatena", pag_token: '', quantity: '', start_time: '', end_time: '2022-10-06T00:00:01Z' }).expect(400);
         expect( res.body.tweets ).not.toBeDefined();
         expect( res.body.next_token ).not.toBeDefined();
     });
