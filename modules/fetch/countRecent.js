fa5cee8690c5c951e8c9a54b290eaf2db319a498
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-11-07 17:04:55 +0100

    Add countRecent documentation

--- modules/fetch/countRecent.js
+++ modules/fetch/countRecent.js
@@ -3,11 +3,16 @@ const axios = require('axios');
 
 module.exports = { getCountRecentHashtagTweets: getCountRecentHashtagTweets }
 
+/** 
+ * Restituisce il numero totale di tweet di un dato hashtag negli ultimi 7 giorni.
+ * @param {string} hashtag      Hashtag da ricercare
+ * @returns {number}            Numero di tweet negli ultimi 7 giorni
+ */
 async function getCountRecentHashtagTweets(hashtag) {
     let options = {
         headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
         params: {
-            query: `${hashtag} has:hashtags -is:reply -is:retweet`,                                         // Filtra per hashtag e rimuove i retweet e le risposte
+            query: `${hashtag} has:hashtags -is:reply -is:retweet`,                                        // Filtra per hashtag e rimuove i retweet e le risposte
         },
     
         validateStatus: () => true
