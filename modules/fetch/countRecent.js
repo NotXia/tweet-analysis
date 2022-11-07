856bc17f5e7ed9386fb1a25f5f8c9aab2d285303
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-11-07 22:16:53 +0100

    Update countRecent

--- modules/fetch/countRecent.js
+++ modules/fetch/countRecent.js
@@ -1,18 +1,18 @@
 require("dotenv").config();
 const axios = require('axios');
 
-module.exports = { getCountRecentHashtagTweets: getCountRecentHashtagTweets }
+module.exports = { getCountRecentKeywordTweets: getCountRecentKeywordTweets }
 
 /** 
  * Restituisce il numero totale di tweet di un dato hashtag negli ultimi 7 giorni.
- * @param {string} hashtag               Hashtag da ricercare
+ * @param {string} keyword               Parola chiave da ricercare
  * @returns {Promise<number>}            Numero di tweet negli ultimi 7 giorni
  */
-async function getCountRecentHashtagTweets(hashtag) {
+async function getCountRecentKeywordTweets(keyword) {
     let options = {
         headers: { Authorization: `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
         params: {
-            query: `${hashtag} has:hashtags -is:reply -is:retweet`,                                        // Filtra per hashtag e rimuove i retweet e le risposte
+            query: `${keyword} -is:reply -is:retweet`,                                        // Filtra per hashtag e rimuove i retweet e le risposte
         },
     
         validateStatus: () => true
