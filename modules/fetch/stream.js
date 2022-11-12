9fd3b92725065dc1042f4cb76144fcc1ff6baceb
NotXia <tiancheng.xia@studio.unibo.it>
2022-11-12 19:31:47 +0100

    Update inserimento rule

--- modules/fetch/stream.js
+++ modules/fetch/stream.js
@@ -105,21 +105,32 @@ async function _getStream(reconnect_attemps=0) {
  * @param {string} username     Nome utente da filtrare
  * @param {string} keyword      Parola chiave da filtrare
  * @throws {ExceededStreamRulesCap}     Se non è possibile inserire la regola a causa del limit cap di Twitter
+ * @returns {string} Id della regola creata
  */
-async function _addRule(username="", keyword="") {
+async function addRule(username="", keyword="") {
     if (!username && !keyword) { throw new Error("Parametri mancanti"); }
 
     const rule = _createRuleString(username, keyword);
 
     try {
         // Creazione della regola
-        await axios({
+        const res = await axios({
             method: "POST", url: "https://api.twitter.com/2/tweets/search/stream/rules",
             headers: { "Authorization": `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
             data: {
                 add: [{ value: rule }]
             }
         });
+
+        if (res.data.data) { // Regola creata correttamente
+            return res.data.data.id; 
+        } 
+        else if (res.data.errors && res.data.errors[0].title === "DuplicateRule") { // Regola già esistente
+            return res.data.errors[0].id;
+        }
+        else {
+            throw new Error();
+        }
     }
     catch (err) {
         if (err.response.data.title === "RulesCapExceeded") { throw new ExceededStreamRulesCap(); }
@@ -127,19 +138,6 @@ async function _addRule(username="", keyword="") {
     }
 }
 
-
-/**
- * Restituisce le regola attualmente attive (in ordine cronologico)
- * @returns {Promise<[{id:string, value:string}]>} Vettore delle regole attualmente attive. id è l'identificativo della regola, value il valore della regola
- */
-async function _getRules() {
-    const res = await axios({
-        method: "GET", url: "https://api.twitter.com/2/tweets/search/stream/rules",
-        headers: { "Authorization": `Bearer ${process.env.TWITTER_BEARER_TOKEN}` },
-    });
-
-    return res.data.data;
-}
 
 /**
  * Gestisce la cancellazione di una regola dato il suo id
@@ -153,21 +151,6 @@ async function deleteRuleById(rule_id) {
             delete: { ids: [rule_id] }
         }
     });
-}
-
-/**
- * Restituisce l'id di una data regola, se esiste
- * @param {string} username     Nome utente della regola
- * @param {string} keyword      Parola chiave della regola
- * @returns {Promise<string>}   Id della regola, se esiste. undefined altrimenti
- */
-async function _getRuleId(username, keyword) {
-    const rules = await _getRules();
-    const rule_value = _createRuleString(username, keyword);
-
-    const found_rule = rules.find((rule) => rule.value === rule_value);
-
-    return found_rule?.id;
 }
 
 
