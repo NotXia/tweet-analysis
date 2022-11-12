b72c93bea46a9e39143aecda7eb4da2356cf8740
NotXia <tiancheng.xia@studio.unibo.it>
2022-11-12 21:46:52 +0100

    Add timeout per le rules inutilizzate

--- sockets/tweetStream.js
+++ sockets/tweetStream.js
@@ -2,6 +2,8 @@ const TweetStream = require("../modules/fetch/stream.js");
 const ExceededStreamRulesCap = require("../modules/fetch/errors/ExceededStreamRulesCap.js");
 
 
+const EMPTY_RULE_TIMEOUT = 10000; // Tempo dopo il quale una regola viene cancellata
+
 module.exports = {
     initTweetStreamSocket: init
 }
@@ -45,6 +47,8 @@ function init(socket) {
 let _sockets_by_rule_id = {};       // Mappa rule_id alla lista dei socket associati
 let _rule_id_by_socket_id = {};     // Mappa socket_id al rule_id associato
 
+function _getSocketsForRule(rule_id) { return _sockets_by_rule_id[rule_id] ?? []; }
+
 /* Gestisce la registrazione di una nuova connessione */
 function _addConnection(socket, rule_id) {
     if (!(rule_id in _sockets_by_rule_id)) { _sockets_by_rule_id[rule_id] = []; }
@@ -56,7 +60,7 @@ function _addConnection(socket, rule_id) {
 /* Inoltra i tweet ai cliente interessati */
 function _forwardTweetToClient(tweet, rules_id) {
     for (const rule_id of rules_id) {
-        _sockets_by_rule_id[rule_id].forEach((socket) => {
+        _getSocketsForRule(rule_id).forEach((socket) => {
             socket.emit("tweet.stream.new", tweet);
         })
     }
@@ -68,11 +72,35 @@ function _disconnectClient(disconnected_socket) {
     _sockets_by_rule_id[to_update_rule_id] = _sockets_by_rule_id[to_update_rule_id].filter((socket) => socket.id != disconnected_socket.id);
 
     delete _rule_id_by_socket_id[disconnected_socket.id];
+
+    _handleRuleTimeout(to_update_rule_id);
 }
 
 /* Gestisce la disconnessione di tutti i client */
 function _disconnectAllClients() {
     for (const sockets of Object.values(_sockets_by_rule_id)) {
         sockets.forEach((socket) => _disconnectClient(socket));
+    }
+}
+
+
+let _delete_request_by_rule_id = {};    // Mappa rule_id alla richiesta di eliminazione della regola
+
+/* Gestisce l'avvio della cancellazione di una regola (se necessario) */
+function _handleRuleTimeout(rule_id) {
+    if (_getSocketsForRule(rule_id).length === 0) {
+        console.log("Timing out " + rule_id)
+        _abortRuleTimeout(rule_id);
+
+        _delete_request_by_rule_id[rule_id] = setTimeout(async () => {
+            await TweetStream.deleteRule(rule_id);
+        }, EMPTY_RULE_TIMEOUT);
+    }
+}
+
+/* Annulla la richiesta di cancellazione per una regola */
+function _abortRuleTimeout(rule_id) {
+    if (_delete_request_by_rule_id[rule_id]) { 
+        clearTimeout(_delete_request_by_rule_id[rule_id]); 
     }
 }
