efb367fabeb6fa866c8c58e6d68d8fb5643ac66a
NotXia <tiancheng.xia@studio.unibo.it>
2022-11-25 07:53:39 +0100

    Fix match mancante con nock

--- tests/unit/multiple_tweets.test.js
+++ tests/unit/multiple_tweets.test.js
@@ -7,40 +7,51 @@ const { multipleTweetsFetch } = require("../../modules/fetch/multiple_tweets.js"
 const { getCountRecentKeywordTweets } = require("../../modules/fetch/countRecent.js");
 const { generateParams, generateTweets, countTweets } = require("../utils/tweet.js");
 
-describe("Test ricerca tweet", function () {
+
+describe("Test conteggio tweet", function () {
     test("Ricerca 40 tweet per hashtag", async function () {
         const query = "#reazioneacatena";
         nock("https://api.twitter.com") 
-            .get('/2/tweets/counts/all').query({query: `%23reazioneacatena%20-is%3Areply%20-is%3Aretweet`})
+            .get('/2/tweets/counts/all').query({query: `${query} -is:reply -is:retweet`})
             .reply(200, {
                 "data": [
-                    {
-                      "end": "2022-10-25T01:00:00.000Z",
-                      "start": "2022-10-25T00:00:00.000Z",
-                      "tweet_count": 10
-                    },
-                    {
-                        "end": "2022-10-25T03:00:00.000Z",
-                        "start": "2022-10-25T02:00:00.000Z",
-                        "tweet_count": 20
-                    },
-                    {
-                        "end": "2022-10-25T01:05:00.000Z",
-                        "start": "2022-10-25T00:04:00.000Z",
-                        "tweet_count": 30
-                    }
+                    { "end": "2022-10-25T01:00:00.000Z", "start": "2022-10-25T00:00:00.000Z", "tweet_count": 10 },
+                    { "end": "2022-10-25T03:00:00.000Z", "start": "2022-10-25T02:00:00.000Z", "tweet_count": 20 },
+                    { "end": "2022-10-25T01:05:00.000Z", "start": "2022-10-25T00:04:00.000Z", "tweet_count": 30 }
                 ],
-                "meta": {
-                    "total_tweet_count": 60
-                }
+                "meta": { "total_tweet_count": 60 }
             });
         const max_results = await getCountRecentKeywordTweets(query);
-        
+        expect(max_results).toEqual(60);
+    });
+});
+
+
+describe("Test ricerca tweet", function () {
+    test("Ricerca 1400 tweet per hashtag", async function () {
+        const query = "#reazioneacatena";
+        const batch1 = generateTweets(500);
+        const batch2 = generateTweets(500);
+        const batch3 = generateTweets(400);
+
+        nock("https://api.twitter.com") 
+            .get('/2/tweets/counts/all').query({query: `${query} -is:reply -is:retweet`})
+            .reply(200, {
+                "data": [ { "end": "2022-10-25T01:00:00.000Z", "start": "2022-10-25T00:00:00.000Z", "tweet_count": 4000 } ],
+                "meta": { "total_tweet_count": 4000 }
+            });
+
         nock("https://api.twitter.com")
-            .get('/2/tweets/search/all').query(generateParams(query))
-            .reply(200, generateTweets(40) );
-        const tweetsPage = await multipleTweetsFetch(getTweetsByKeyword, query, "", 40);
-        expect( tweetsPage.tweets.length ).toBeLessThanOrEqual(max_results);
+            .get('/2/tweets/search/all').query(generateParams(query, "", 500))
+            .reply(200, batch1);
+        nock("https://api.twitter.com")
+            .get('/2/tweets/search/all').query(generateParams(query, batch1.meta.next_token, 500))
+            .reply(200, batch2);
+        nock("https://api.twitter.com")
+            .get('/2/tweets/search/all').query(generateParams(query, batch2.meta.next_token, 400))
+            .reply(200, batch3);
+        const tweetsPage = await multipleTweetsFetch(getTweetsByKeyword, query, "", 1400);
+        expect( tweetsPage.tweets.length ).toEqual(1400);
         for (const tweet of tweetsPage.tweets) {
             expect( tweet.name ).toBeDefined();
             expect( tweet.username ).toBeDefined();
