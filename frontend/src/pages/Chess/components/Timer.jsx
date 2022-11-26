561809400cce6ad38e7d42505812f65225a14bfd
NotXia <tiancheng.xia@studio.unibo.it>
2022-11-26 19:30:30 +0100

    Add timer

---
+++ frontend/src/pages/Chess/components/Timer.jsx
@@ -0,0 +1,52 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+
+class Timer extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            time: 0
+        };
+
+        this.current_decrease = null;
+    }
+
+    render() {
+        return (
+            <span className="fs-1 fw-bold">
+                { this._msToTime(this.state.time) }
+            </span>
+        );
+    }
+
+    _msToTime(ms) {
+        let minutes = Math.floor(ms / 60000);
+        let seconds = ((ms % 60000) / 1000).toFixed(0);
+        return `${String(minutes).padStart(2, "0")}:${String(seconds).padStart(2, "0")}`;
+    }
+
+    _decrease() {
+        let time = this.state.time-1000;
+        if (time < 0) { time = 0; }
+
+        this.setState({ time: time }, () => {
+            if (this.state.time > 0) {
+                this.current_decrease = setTimeout(() => {
+                    this._decrease();
+                }, 1000);
+            }
+        })
+    }
+
+    setTime(ms) {
+        if (ms < 0) { return; }
+        clearTimeout(this.current_decrease);
+
+        this.setState({ time: ms+1000 }, () => {
+            this._decrease();
+        })
+    }
+}
+
+export default Timer;
--- frontend/src/pages/Chess/index.jsx
+++ frontend/src/pages/Chess/index.jsx
@@ -5,6 +5,7 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import Navbar from "../../components/Navbar"
 import ChessGame from "../../modules/games/ChessGame"
 import Board from "./components/Board"
+import Timer from "./components/Timer"
 
 
 class Test extends React.Component {
@@ -12,17 +13,19 @@ class Test extends React.Component {
         super(props);
         this.state = {
             game_ready: false,
-            board_width: (Math.min(window.innerHeight, window.innerWidth)) * 0.7
+            board_width: (Math.min(window.innerHeight, window.innerWidth)) * 0.75,
+            current_color: "black"
         };
 
         this.board = React.createRef();
+        this.timer = React.createRef();
 
         this.onBoardMove = this.onBoardMove.bind(this);
     }
 
     componentDidMount() {
         window.onresize = (e) => {
-            this.setState({ board_width: (Math.min(window.innerHeight, window.innerWidth)) * 0.7 })
+            this.setState({ board_width: (Math.min(window.innerHeight, window.innerWidth)) * 0.75 })
         };
     }
 
@@ -32,21 +35,53 @@ class Test extends React.Component {
                 <title>Scacchi</title>
             </Helmet>
 
-            <Navbar />
-            
-            <div className={`${this.state.game_ready ? "d-none" : ""} d-flex align-items-center justify-content-center mt-3`}>
-                <button className="btn btn-outline-success btn-lg" onClick={() => this.stateGame()}>Inizia una partita</button>
-            </div>
+            <div style={{ display: "flex", flexFlow: "column", height: "100vh" }}>
+                <div style={{ flex: "0 1 auto" }}>
+                    <Navbar />
+                </div>
+                
+                <main style={{ flex: "1 1 auto" }}>
+                    <div className="d-flex justify-content-center align-items-center h-100">
+                        {/* Schermata iniziale */}
+                        <div className={`${this.state.game_ready ? "d-none" : ""}`}>
+                            <button className="btn btn-outline-success btn-lg" onClick={() => this.stateGame()}>Inizia una partita</button>
+                        </div>
 
-            <div className={`${this.state.game_ready ? "" : "d-none"} d-flex justify-content-center mt-3`}>
-                <Board ref={this.board} id="BasicBoard" onMove={this.onBoardMove} width={this.state.board_width} />
+                        {/* Partita */}
+                        <div className={`${this.state.game_ready ? "" : "d-none"} w-100`}>
+                            <div className="container-fluid">
+                                <div className="row">
+                                    <div className="col-12 col-md-6">
+                                        <div className="d-flex justify-content-center justify-content-md-end">
+                                            <Board ref={this.board} id="BasicBoard" onMove={this.onBoardMove} width={this.state.board_width} />
+                                        </div>
+                                    </div>
+
+                                    <div className="col-12 col-md-6">
+                                        <div className="d-flex justify-content-evenly">
+                                            <div className={`${this.state.current_color === "white" ? "" : "invisible"}`}>
+                                                <img src={`${process.env.PUBLIC_URL}/icons/chess/king_white.png`} alt="Turno bianchi" />
+                                            </div>
+                                            <Timer ref={this.timer} />
+                                            <div className={`${this.state.current_color === "black" ? "" : "invisible"}`}>
+                                                <img src={`${process.env.PUBLIC_URL}/icons/chess/king_black.png`} alt="Turno neri" />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </main>
             </div>
         </>);
     }
 
     async stateGame() {
         const onTurnStart = (player, timer) => {
-            console.log(player, timer)
+            this.setState({ current_color: this.state.current_color === "white" ? "black" : "white"  });
+            this.timer.current.setTime(timer);
         }
         const onMove = (player, move, fen) => {
             this.setState({ fen: fen });
