2d62287b746f8831b0a99885d08f28485a4fc56d
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-11-27 17:51:36 +0100

    Fix

--- modules/games/winningWord.js
+++ modules/games/winningWord.js
@@ -18,7 +18,7 @@ async function getWinningWord(date="", search="La #parola della #ghigliottina de
         let tweetText = word.tweets[0].text.replace(/(\r\n|\n|\r)/gm, " ");         // Viene modificata la stringa togliendo gli "a capo"
         tweetText = tweetText.replace(/\s\s+/g, " ");                               // Vengono collassati tutti gli spazi in uno solo
 
-        var result = tweetText.match(new RegExp(`${search}\\s(\\w+)`))[1];          // Ricerca parola vincente
+        const result = tweetText.match(new RegExp(`${search}\\s(\\w+)`))[1];          // Ricerca parola vincente
 
         return result
     } catch (err) {
