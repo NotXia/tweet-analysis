7d10dd9bc07487867c79b81f94938b1994d2d75a
Zaid1710 <zaid.cheikhibrahim@studio.unibo.it>
2022-12-01 19:19:39 +0100

    Update router

--- routes/games.js
+++ routes/games.js
@@ -4,7 +4,12 @@ const router = express.Router();
 const middlewareGhigliottina = require("../middlewares/ghigliottina.js");
 const controllerGhigliottina = require("../controllers/ghigliottina.js");
 
+const middlewareCatenaFinale = require("../middlewares/catenafinale.js");
+const controllerCatenaFinale = require("../controllers/catenafinale.js");
+
 router.get("/ghigliottina/winning_word", middlewareGhigliottina.ghigliottinaWinningWordValidation, controllerGhigliottina.ghigliottinaWinningWord);
 router.get("/ghigliottina", middlewareGhigliottina.gamesGhigliottinaValidation, controllerGhigliottina.gamesGhigliottina);
+
+router.get("/catenafinale/winning_word", middlewareCatenaFinale.catenaFinaleWinningWordValidation, controllerCatenaFinale.catenaFinaleWinningWord);
 
 module.exports = router;
