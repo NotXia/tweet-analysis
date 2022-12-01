18633719667c2ef9d25bb1aeeb548a402ae9d97f
lee_henry <qunhaohaohenry.lee@studio.unibo.it>
2022-12-01 21:23:45 +0100

    Modulo catenafinale

---
+++ models/CatenaFinale.js
@@ -0,0 +1,85 @@
+require('dotenv').config();
+const mongoose = require("mongoose");
+const consts = require("./utils/consts.js");
+
+
+const tweet_scheme = mongoose.Schema ({
+    _id: { type: String, required: true },
+    date: String,
+    tweet: {
+        id: { type: String, required: true },
+        name: String,
+        username: String,
+        pfp: String,
+        text: String,
+        time: String,
+        likes: Number,
+        comments: Number,
+        retweets: Number,
+        location: {
+            id: String,
+            full_name: String,
+            country: String,
+            coords: {
+                long: String,
+                lat: String
+            }
+        },
+        media: [
+            new mongoose.Schema({
+                url: String,
+                type: String
+            }, { _id: false })
+        ]
+    },
+    word: String
+});
+
+
+/**
+ * Gestisce il salvataggio di un tweet
+ * @param {Object} tweet    Tweet da salvare
+ */
+tweet_scheme.statics.cacheTweet = async function(tweet, date) {
+    if (process.env.NODE_ENV.includes("testing")) { return; }
+
+    try {
+        await new this({
+            _id: tweet.tweet.id,
+            date: date,
+            tweet: {
+                id: tweet.tweet.id,
+                name: tweet.tweet.name,
+                username: tweet.tweet.username,
+                pfp: tweet.tweet.pfp,
+                text: tweet.tweet.text,
+                time: tweet.tweet.time,
+                likes: tweet.tweet.likes,
+                comments: tweet.tweet.comments,
+                retweets: tweet.tweet.retweets,
+                location: tweet.tweet.location,
+                media: tweet.tweet.media
+            },
+            word: tweet.word
+        }).save();
+    }
+    catch (err) {
+        if (err.code === consts.MONGO_DUPLICATED_KEY) { return; } // Tweet già salvato
+        throw err;
+    }
+};
+
+tweet_scheme.statics.cacheCatenaFinale = async function(date) {
+    if (process.env.NODE_ENV.includes("testing")) { return null; }
+
+    try {
+        let tweets = await this.find({ date: date });
+        return tweets;
+    }
+    catch (err) {
+        return null;
+    }
+};
+
+
+module.exports = mongoose.model("tweets_catenaFinale", tweet_scheme);
--- routes/games.js
+++ routes/games.js
@@ -4,7 +4,13 @@ const router = express.Router();
 const middlewareGhigliottina = require("../middlewares/ghigliottina.js");
 const controllerGhigliottina = require("../controllers/ghigliottina.js");
 
+const middlewareCatenaFinale = require("../middlewares/catenaFinale.js");
+const controllerCatenaFinale = require("../controllers/catenaFinale.js");
+
 router.get("/ghigliottina/winning_word", middlewareGhigliottina.ghigliottinaWinningWordValidation, controllerGhigliottina.ghigliottinaWinningWord);
 router.get("/ghigliottina", middlewareGhigliottina.gamesGhigliottinaValidation, controllerGhigliottina.gamesGhigliottina);
+
+router.get("/catenaFinale/winning_word", middlewareCatenaFinale.catenaFinaleWinningWordValidation, controllerCatenaFinale.catenaFinaleWinningWord);
+router.get("/catenaFinale", middlewareCatenaFinale.gamesCatenaFinaleValidation, controllerCatenaFinale.gamesCatenaFinale);
 
 module.exports = router;
