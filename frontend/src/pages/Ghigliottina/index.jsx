e111abe950f4a4edcc0dda306e36e81c9d7034b0
Manuel Paris <manuel.paris@studio.unibo.it>
2022-12-01 17:01:52 +0100

    Update pagina ghigliottina
    Co-authored-by: Tian Cheng Xia <tiancheng.xia@studio.unibo.it>
    Co-authored-by: Qun Hao Henry Lee <qunhaohaohenry.lee@studio.unibo.it>

--- frontend/src/pages/Ghigliottina/index.jsx
+++ frontend/src/pages/Ghigliottina/index.jsx
@@ -6,6 +6,7 @@ import Navbar from "../../components/Navbar";
 import { keywordSearchTweet } from "../../modules/fetch-tweets/search_keyword.js";
 import Tweet from "../../components/Tweet";
 import GeolocationMap from "../../components/maps/GeolocationMap";
+import { getGhigliottinaAttempts, getGhigliottinaWord } from "../../modules/games/ghigliottinaGame";
 import moment from "moment";
 
 /**
@@ -15,7 +16,7 @@ const today = new Date();
 const date = (today.toISOString()).split("T");
 
 const __max_date_limit = date[0];
-const __min_date_limit = "2010-11-06";
+const __min_date_limit = "2006-03-23";
 
 
 class SearchTweets extends React.Component {
@@ -24,14 +25,13 @@ class SearchTweets extends React.Component {
         this.state = {
             tweets: [],                             // Array dei tweet 
             date: "",                               // Data da cercare
+            winning_word: "",                       // Parola vincente
             fetching: false,                        // Indica se attualmente si sta richiedendo dei tweet
 
             select_min_date: __min_date_limit,      // Limite minimo attuale della data (a init: "2010-11-06")
             select_max_date: __max_date_limit,      // Limite massimo attuale della data (a init: data di oggi)
             error_message: ""
         };
-
-        this.tweets_buffer = [];
     }
 
     render() {
@@ -58,7 +58,7 @@ class SearchTweets extends React.Component {
                             <div className="list-group border border-white rounded-4">
                             {
                                 this.state.tweets.map((tweet) => (
-                                    <Tweet key={tweet.id} tweet={tweet} />
+                                    <Tweet key={tweet.tweet.id} tweet={tweet.tweet} />
                                 ))
                             }
                             </div>
@@ -78,13 +78,22 @@ class SearchTweets extends React.Component {
                                         </div>
                                     </form>
                                 </div>
+
+                                {
+                                    this.state.winning_word !== "" &&
+                                    <div className="d-flex justify-content-center mt-2">
+                                        <div className="border rounded fs-3 p-2 px-4">
+                                            { this.state.winning_word }
+                                        </div>
+                                    </div>
+                                }
                             </div>
                         </div>
                         {/* Mappa geolocalizzazione */}
                         <div className="col-12 order-2 col-md-6 order-md-2 col-lg-3">
                             <div className="sticky-top">
                                 <div className={`${this.state.tweets.length === 0 ? "d-none" : ""}`} style={{height: "97vh"}}>
-                                    <GeolocationMap tweets={this.state.tweets} cluster={true} connect={false} />
+                                    <GeolocationMap tweets={this.state.tweets.map((tweet) => tweet.tweet)} cluster={true} connect={false} />
                                 </div>
                             </div>
                         </div>
@@ -98,53 +107,26 @@ class SearchTweets extends React.Component {
      * Funzione richiamata al submit del form
      */
     async searchTweets(date) {
-    
+        date = `${date}T00:00:00Z`;
+
         try {
+            this.setState({ fetching: true }); // Inizio fetching
 
-            const start_date = date ? moment(date, "YYYY-MM-DD").startOf("day").utc().format() : "";
-            let end_date = date ? moment(date, "YYYY-MM-DD").endOf("day").utc().format() : "";
-            this.tweets_buffer = [];
-
-            // Se la data supera la data odierna, viene impostata la data odierna (10 secondi sottratti per necessità delle API di Twitter)
-            if (end_date && moment(end_date).isAfter(moment())) { end_date = moment().subtract(10, "seconds").utc().format(); } 
-
-            let tweets_data = await this.fetchTweets(start_date, end_date);
+            let tweets_data = await getGhigliottinaAttempts(date);
+            const winning_word = await getGhigliottinaWord(date);
 
             this.setState({ 
                 tweets: tweets_data.tweets,
                 date: date,
+                winning_word: winning_word.word,
+                
+                fetching: false,
                 error_message:""
             })
         }
         catch (err) {
             this.setState({ error_message: "Si è verificato un errore durante la ricerca" });
         }
-    }
-
-    /**
-     * Ricerca i tweet inerenti alla richiesta
-     * @param {string} start_date           Data inizio di ricerca
-     * @param {string} end_date             Data finale di ricerca
-     * @returns {Promise <object[]>}        Array dei tweet trovati
-     */
-    async fetchTweets(start_date="", end_date="") {
-
-        let fetched_tweets = [];
-        let tweets_data = { 
-            tweets: [],
-            start_date: "",
-            end_date: ""
-        };
-        
-        this.setState({ fetching: true }); // Inizio fetching
-
-        tweets_data = await keywordSearchTweet("#leredita", "", "", start_date, end_date);
-        fetched_tweets = fetched_tweets.concat(tweets_data.tweets);
-
-        this.setState({ fetching: false }); // Termine fetching
-
-        tweets_data.tweets = fetched_tweets;
-        return tweets_data;
     }
     
 }
