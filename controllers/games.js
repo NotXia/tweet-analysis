4e000115d876efaeb43aa54ba205ea9cd7bd94a8
NotXia <tiancheng.xia@studio.unibo.it>
2022-12-02 10:01:30 +0100

    Add controller giochi generico

---
+++ controllers/games.js
@@ -0,0 +1,76 @@
+const { getWinningWord } = require("../modules/games/winningWord.js");
+const WordModel = require("../models/WinningWord.js");
+const TVGameModel = require("../models/TVGame.js");
+
+
+/**
+ * Genera il controller per estrarre la parola vincente di un dato gioco
+ * @param {String} game_name         Nome del gioco
+ * @param {String} query_phrase      Frase che precede la parola vincente
+ * @param {String} query_user        Utente da cui cercare la parola vincente
+ */
+function winningWord(game_name, query_phrase, query_user) {
+    return async function (req, res) {
+        let winning_word = {};
+    
+        try {
+            winning_word = await WordModel.getWordOfDay(req.query.date, game_name);
+    
+            if (!winning_word) {
+                winning_word = await getWinningWord(req.query.date, query_phrase, query_user);
+            }
+        } catch (error) {
+            if (error.message === "Tweet non trovato") { return res.sendStatus(404); }
+            res.sendStatus(500);
+            return;
+        }
+    
+        res.status(200).json({
+            word: winning_word.word,
+            date: winning_word.date
+        });
+    
+        if (!process.env.NODE_ENV.includes("testing")) {
+            // Caching tweet
+            await WordModel.cacheWord(winning_word, game_name);
+        }
+    }
+}
+
+/**
+ * Genera il controller per estrarre i tentativi di indovinare la parola di un dato gioco
+ * @param {function} tweet_fetcher     Funzione che estrae i tweet
+ * @param {String} game_name         Nome del gioco
+ */
+function userAttempts(tweet_fetcher, game_name) {
+    return async function gamesGhigliottina(req, res) {
+        let tweets_response;
+        let should_cache = false;
+    
+        try {
+            tweets_response = await TVGameModel.getCache(game_name, req.query.date);
+    
+            if (!tweets_response || tweets_response.length === 0) {
+                should_cache = true;
+                tweets_response = await tweet_fetcher(req.query.date);
+            }
+        } catch (error) {
+            res.sendStatus(500);
+            return;
+        }
+    
+        res.status(200).json({ tweets: tweets_response });
+    
+        if (!process.env.NODE_ENV.includes("testing")) {
+            // Caching tweet
+            if (should_cache) {
+                await Promise.all(tweets_response.map(async (tweet) => TVGameModel.cacheTweet(tweet, game_name, req.query.date)));
+            }
+        }
+    }
+}
+
+module.exports = {
+    userAttempts: userAttempts,
+    winningWord: winningWord
+};
--- routes/games.js
+++ routes/games.js
@@ -2,14 +2,16 @@ const express = require("express");
 const router = express.Router();
 
 const middlewareGames = require("../middlewares/games.js");
+const controllerGames = require("../controllers/games.js");
 
-const controllerGhigliottina = require("../controllers/ghigliottina.js");
-const controllerCatenaFinale = require("../controllers/catenafinale.js");
+const { ghigliottina } = require("../modules/games/ghigliottina.js");
+const { catenaFinale } = require("../modules/games/catenafinale.js");
 
-router.get("/ghigliottina/winning_word", middlewareGames.gamesWinningWordValidation, controllerGhigliottina.ghigliottinaWinningWord);
-router.get("/ghigliottina", middlewareGames.gamesValidation, controllerGhigliottina.gamesGhigliottina);
 
-router.get("/catenafinale/winning_word", middlewareGames.gamesWinningWordValidation, controllerCatenaFinale.catenaFinaleWinningWord);
-router.get("/catenaFinale", middlewareGames.gamesValidation, controllerCatenaFinale.gamesCatenaFinale);
+router.get("/ghigliottina/winning_word", middlewareGames.gamesWinningWordValidation, controllerGames.winningWord("l'eredita", "La #parola della #ghigliottina de #leredita di oggi è:", "quizzettone"));
+router.get("/ghigliottina", middlewareGames.gamesValidation, controllerGames.userAttempts(ghigliottina, "l'eredita"));
+
+router.get("/catenafinale/winning_word", middlewareGames.gamesWinningWordValidation, controllerGames.winningWord("reazione a catena", "La #parola della #catena finale per #reazioneacatena di oggi è:", "quizzettone"));
+router.get("/catenaFinale", middlewareGames.gamesValidation, controllerGames.userAttempts(catenaFinale, "reazione a catena"));
 
 module.exports = router;
