d55f87ea242fc4f9ed0f65b2440fd76322a5df38
NotXia <tiancheng.xia@studio.unibo.it>
2022-12-05 09:01:42 +0100

    Remove await non necessari

--- modules/chess/ChessOpponent.js
+++ modules/chess/ChessOpponent.js
@@ -1,9 +1,6 @@
-const path = require("path");
-const { nanoid } = require("nanoid");
 const { sendTweet } = require("../tweet/send.js");
 const { deleteTweet } = require("../tweet/delete.js");
 const { getAllRepliesOf } = require("../fetch/reply.js");
-const fs = require("fs").promises;
 var ChessImageGenerator = require("chess-image-generator");
 const { Chess } = require("chess.js");
 const Jimp = require("jimp");
@@ -51,21 +48,17 @@ async function generateBoardImage(fen) {
     await image_generator.loadFEN(fen);
 
     // Aggiunta padding
-    let base_image = await new Jimp(870, 870, "white")
+    let base_image = new Jimp(870, 870, "white");
     let board_image = await Jimp.read(await image_generator.generateBuffer());
-    board_image = await base_image.composite(board_image, 35, 35);
+    board_image = base_image.composite(board_image, 35, 35);
     
     // Generazione coordinate caselle
     let font = await Jimp.loadFont(Jimp.FONT_SANS_32_BLACK);
     const letters = [ "a", "b", "c", "d", "e", "f", "g", "h" ] ;   
-    for (let i=0; i<8; i++) { board_image = await board_image.print(font, 10, 100*i+65, 8-i); }
-    for (let i=0; i<8; i++) { board_image = await board_image.print(font, 840, 100*i+65, 8-i); }
-    for (let i=0; i<8; i++) { board_image = await board_image.print(font, 100*i+78, 833, letters[i]); }
-    for (let i=0; i<8; i++) { board_image = await board_image.print(font, 100*i+78, 0, letters[i]); }
+    for (let i=0; i<8; i++) { board_image = board_image.print(font, 10, 100*i+65, 8-i); }
+    for (let i=0; i<8; i++) { board_image = board_image.print(font, 840, 100*i+65, 8-i); }
+    for (let i=0; i<8; i++) { board_image = board_image.print(font, 100*i+78, 833, letters[i]); }
+    for (let i=0; i<8; i++) { board_image = board_image.print(font, 100*i+78, 0, letters[i]); }
     
-    // await board_image.write(path);
-    // await new Promise(r => setTimeout(r, 200));
     return await board_image.getBufferAsync(Jimp.MIME_PNG);
 }
-
-// generateBoardImage("rnbqkbnr/pp1ppppp/8/2p5/5P2/8/PPPPP1PP/RNBQKBNR b KQkq f3 0 1", "D:/Uni_dev/tmp/prova.png")
